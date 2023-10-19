86503b2bdc7fbe76c3ad35a653e3b278ae38ee54
Martin Enriquez <martinantraxelm1@gmail.com>
2023-10-19 14:09:46 -0700

    finished working on min_max

---
+++ IfScripts/min_max.js
@@ -0,0 +1,35 @@
+// displays the largest number
+
+const a = 14;
+const b = 11;
+const c = 14;
+
+let displayValue = "";
+
+if (c >= b && c >= a) {
+    displayValue = "The largest number is " + c ;
+}
+else if (b >= a) {
+    displayValue = "The largest number is " + b;
+}
+else {
+    displayValue = "The largest number is " + a;
+}
+
+console.log(displayValue);
+
+
+// displays the smallest number 
+let displayNumber = "";
+
+if (c <= b && c <= a) {
+    displayNumber = "The smallest number is " + c ;
+}
+else if (b <= a) {
+    displayNumber = "The smallest number is " + b;
+}
+else {
+    displayNumber = "The smallest number is " + a;
+}
+
+console.log(displayNumber);
